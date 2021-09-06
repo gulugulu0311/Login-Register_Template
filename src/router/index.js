bf5e78a5c2ebc31d6e46ee08a9ad6221a4f1fb92
gulugulu0311 <1079713055@qq.com>
2021-09-06 19:47:59 +0800

    foo

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import loginRegister from "../views/loginRegister.vue";
+import Map from "../views/BaiduMap.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: loginRegister,
+  },
+  {
+    path: "/map",
+    name: "map",
+    component: Map,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ src/views/BaiduMap.vue
@@ -0,0 +1,46 @@
+<template>
+  <baidu-map class="bm-view" v-bind="mapOptions">
+    <bm-control :offset="{ width: '10px', height: '10px' }">
+      <bm-auto-complete v-model="keyword" :sugStyle="{ zIndex: 1 }">
+      </bm-auto-complete>
+    </bm-control>
+    <bm-local-search :keyword="keyword" :auto-viewport="true"></bm-local-search>
+  </baidu-map>
+</template>
+ 
+<script>
+import {
+  BaiduMap,
+  BmControl,
+  BmView,
+  BmAutoComplete,
+  BmLocalSearch,
+} from "vue-baidu-map";
+export default {
+  components: {
+    BaiduMap,
+    BmView,
+    BmControl,
+    BmAutoComplete,
+    BmLocalSearch,
+  },
+  data() {
+    return {
+      mapOptions: {
+        ak: "ak",
+        center: "广州",
+        scrollWheelZoom: true,
+      },
+      keyword: "",
+    };
+  },
+  methods: {},
+};
+</script>
+ 
+<style>
+.bm-view {
+  width: 100%;
+  height: 100%;
+}
+</style>
